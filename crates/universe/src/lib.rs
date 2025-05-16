599af79bde40e8d958d4b1b42cf652a8e5ca5927
x4132 <31595285+x4132@users.noreply.github.com>
2025-05-15 23:09:58 -0700

    Revert "fix: add proper from() syntax"
    This reverts commit 65de3f8792a1fe8270536d38583af225eb8733ab.

--- crates/universe/src/lib.rs
+++ crates/universe/src/lib.rs
@@ -6,91 +6,6 @@ use serde::Deserialize;
 
 mod macros;
 
-/*
-ID RANGES
-https://developers.eveonline.com/docs/guides/id-ranges/
-| From          | To            | Description                                                          |
-|---------------|---------------|----------------------------------------------------------------------|
-| 0             | 9,999         | System items (including junkyards and other special purpose items)   |
-| 500,000       | 599,999       | Factions                                                             |
-| 1,000,000     | 1,999,999     | NPC corporations                                                     |
-| 3,000,000     | 3,999,999     | NPC characters (agents and NPC corporation CEOs)                     |
-| 9,000,000     | 9,999,999     | Universes                                                            |
-| 10,000,000    | 19,999,999    | Regions                                                              |
-| 10,000,000    | 10,999,999    | New Eden (known space) regions                                       |
-| 11,000,000    | 11,999,999    | Wormhole regions                                                     |
-| 12,000,000    | 12,999,999    | Abyssal regions                                                      |
-| 14,000,000    | 14,999,999    | Void regions                                                         |
-| 20,000,000    | 29,999,999    | Constellations                                                       |
-| 20,000,000    | 20,999,999    | New Eden (known space) constellations                                |
-| 21,000,000    | 21,999,999    | Wormhole constellations                                              |
-| 22,000,000    | 22,999,999    | Abyssal constellations                                               |
-| 24,000,000    | 24,999,999    | Void constellations                                                  |
-| 30,000,000    | 39,999,999    | Solar systems                                                        |
-| 30,000,000    | 30,999,999    | New Eden (known space) solar systems                                 |
-| 31,000,000    | 31,999,999    | Wormhole solar systems                                               |
-| 32,000,000    | 32,999,999    | Abyssal systems                                                      |
-| 34,000,000    | 34,999,999    | Void systems                                                         |
-| 40,000,000    | 49,999,999    | Celestials (suns, planets, moons, asteroid belts)                    |
-| 50,000,000    | 59,999,999    | Stargates                                                            |
-| 60,000,000    | 63,999,999    | Stations                                                             |
-| 60,000,000    | 60,999,999    | Stations created by CCP                                              |
-| 61,000,000    | 63,999,999    | Stations created from outposts                                       |
-| 66,000,000    | 67,999,999    | Station folders of corporation offices                               |
-| 68,000,000    | 68,999,999    | Station folders for stations created by CCP                          |
-| 69,000,000    | 69,999,999    | Station folders for stations created from outposts                   |
-| 70,000,000    | 79,999,999    | Asteroids                                                            |
-| 80,000,000    | 80,099,999    | Control Bunkers                                                      |
-| 81,000,000    | 81,999,999    | WiS Promenades                                                       |
-| 82,000,000    | 84,999,999    | Planetary Districts                                                  |
-| 90,000,000    | 97,999,999    | EVE characters created between 2010-11-03 and 2016-05-30             |
-| 98,000,000    | 98,999,999    | EVE corporations created after 2010-11-03                            |
-| 99,000,000    | 99,999,999    | EVE alliances created after 2010-11-03                               |
-| 100,000,000   | 2,099,999,999 | EVE characters, corporations and alliances created before 2010-11-03 |
-| 2,100,000,000 | 2,111,999,999 | DUST characters, EVE characters created after 2016-05-30             |
-| 2,112,000,000 | 2,129,999,999 | EVE characters created after 2016-05-30                              |
-*/
-#[derive(Clone, Deserialize, PartialEq, Debug)]
-pub struct RegionId(u32);
-
-impl RegionId {
-    pub fn build(id: u32) -> Option<Self> {
-        if id >= 10_000_000 && id <= 19_999_999 {
-            Some(RegionId(id))
-        } else {
-            None
-        }
-    }
-}
-
-#[derive(Clone, Deserialize, PartialEq, Debug)]
-pub struct SystemId(u32);
-
-impl SystemId {
-    pub fn build(id: u32) -> Option<Self> {
-        // Solar systems range: 30,000,000 - 39,999,999
-        if id >= 30_000_000 && id <= 39_999_999 {
-            Some(SystemId(id))
-        } else {
-            None
-        }
-    }
-}
-
-#[derive(Clone, Deserialize, PartialEq, Debug)]
-pub struct StationId(u32);
-
-impl StationId {
-    pub fn build(id: u32) -> Option<Self> {
-        // Stations range: 60,000,000 - 63,999,999
-        if id >= 60_000_000 && id <= 63_999_999 {
-            Some(StationId(id))
-        } else {
-            None
-        }
-    }
-}
-
 /// This struct represents a geospatial point in the EvE universe.
 #[derive(Clone, Debug, Copy, Deserialize, PartialEq)]
 pub struct Point {
@@ -152,28 +67,13 @@ impl Region {
         Ok(region)
     }
 }
-impl From<RegionAPIRequest> for Region {
-    /// Converts a serialized region ESI request into the region struct.
-    /// # Examples
-    /// ```
-    /// use universe::{Region, RegionAPIRequest};
-    /// let region = Region::from(RegionAPIRequest {region_id: 10000002, name: String::from("The Forge")});
-    /// assert_eq!(region, Region {id: 10000002, name: String::from("The Forge")});
-    /// ```
-    fn from(api_request: RegionAPIRequest) -> Self {
-        Region {
-            id: api_request.region_id,
-            name: api_request.name,
-        }
-    }
-}
 
 type RegionResult = Result<Region, Box<dyn Error>>;
 
 #[derive(Deserialize)]
-pub struct RegionAPIRequest {
-    pub region_id: u32,
-    pub name: String,
+struct RegionAPIRequest {
+    region_id: u32,
+    name: String,
 }
 
 #[derive(Clone, Deserialize, PartialEq, Debug)]
