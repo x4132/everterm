ad40650cb2726d386f89bc4eaaa4280eae8c4e11
x4132 <31595285+x4132@users.noreply.github.com>
2025-05-21 16:12:48 -0700

    go back to regular vite SPA lol

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import CommandInput from "@/components/CommandInput.vue";
+</script>
+
+<template ">
+<div class="dark w-full h-full dark:">
+    <CommandInput />
+</div>
+</template>
+
+<style scoped></style>
---
+++ client/src/components/CommandInput.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Input } from "@/components/ui/input";
+
+</script>
+
+<template>
+<Input />
+</template>
+
+<style scoped>
+</style>
