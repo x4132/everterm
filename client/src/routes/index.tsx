f7cf258dcb91a9a99bcbd5d5e1e07a95d57ce170
x4132 <31595285+x4132@users.noreply.github.com>
2025-05-24 14:31:04 -0700

    add tauri back

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -7,6 +7,7 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className="dark bg-background text-primary h-full w-full">
+      <h1 className="text-4xl" >Everterm</h1>
     </div>
   )
 }
