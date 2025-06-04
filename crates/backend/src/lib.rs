086191ddfb2d0e17d965d46198fa64c73e6c2f9d
x4132 <31595285+x4132@users.noreply.github.com>
2025-06-03 19:45:58 -0700

    begin work on backend structure name resolution

--- crates/backend/src/lib.rs
+++ crates/backend/src/lib.rs
@@ -1,8 +1,12 @@
 use axum::{Router, extract::Path, routing::get};
+use esi::ESIClient;
 
 static DATAFETCH_URL: std::sync::LazyLock<String> = std::sync::LazyLock::new(|| {
     std::env::var("DATAFETCH_URL").unwrap_or(String::from("http://0.0.0.0:6380"))
 });
+
+static ESI_CLIENT: std::sync::LazyLock<ESIClient> =
+    std::sync::LazyLock::new(|| ESIClient::new("backend", std::env::consts::OS, 8));
 
 pub fn market_data() -> Router {
     Router::new()
@@ -11,6 +15,7 @@ pub fn market_data() -> Router {
             get(|| async { format!("OK {}", chrono::Utc::now().to_rfc2822()) }),
         )
         .route("/orders/{id}", get(get_order))
+        .route("/universe/struct_names/", get(get_structures))
 }
 
 async fn get_order(Path(id): Path<String>) -> String {
@@ -20,4 +25,9 @@ async fn get_order(Path(id): Path<String>) -> String {
         .text()
         .await
         .unwrap()
+}
+
+async fn get_structures() -> String {
+    if ESI_CLIENT.
+    String::from("{}")
 }
